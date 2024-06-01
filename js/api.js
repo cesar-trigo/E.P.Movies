044e7d274440069b8fb4747bdb14d3da316ee43f
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-06-01 19:33:01 -0300

    se creo scrip para consumir datos de la api

--- js/api.js
+++ js/api.js
@@ -1,7 +1,10 @@
+const $contentCardApi = document.getElementById("contentCardApi");
+
 async function getAnimes() {
   try {
     const res = await fetch("https://api.jikan.moe/v4/anime");
     const data = await res.json();
+    imprCards(data.data, $contentCardApi);
     console.log(data.data);
   } catch (error) {
     console.error(error);
@@ -9,3 +12,25 @@ async function getAnimes() {
 }
 
 getAnimes();
+
+const createCards = data => {
+  const card = document.createElement("div");
+  card.innerHTML = `
+  <img class="img-card-api" src=${data.images.webp.image_url} alt=${data.title}>
+  <div>
+    <p>Finalizado</p>
+    <p>${data.type}</p>
+    <p>${data.title}</p>
+  </div>
+  `;
+  return card;
+};
+
+const imprCards = (data, cont) => {
+  cont.innerHTML = ``;
+  if (data.length > 0) {
+    let fragment = document.createDocumentFragment();
+    data.forEach(e => fragment.appendChild(createCards(e, cont)));
+    cont.appendChild(fragment);
+  }
+};
