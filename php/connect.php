e7ba479bf8b44640eb461d4f527f7593d32149a8
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-07-05 11:10:40 -0300

    se pasaron los parametros de la db

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,15 @@
 <?php //conexion a la base de datos
-$host="";
-$user="";
+$host="localhost";
+$user="root";
 $pass="";
-$baseDatos="";
+$baseDatos="codoacodo";
+
+//variablees de la base de datos HOST
+/* $host="localhost";
+$user="id22368874_tinto01";
+$pass="Php@4457";
+$baseDatos="id22368874_cacphp"; */
+
 
 $connect = mysqli_connect($host, $user, $pass, $baseDatos);
 
